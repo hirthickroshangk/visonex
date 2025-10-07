79df064d0c868431f2fb6c6b1fa34cae3ccbc890
hirthickroshangk <roshan7hirthick@gmail.com>
2025-10-07 15:15:49 +0530

    update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,17 @@
+// src/app/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import { tmdbApi } from '../services/TMDB';
 import genreOrCategoryReducer from '../features/currentGenreOrCategory';
 import authUserReducer from '../features/authUser';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
     currentGenreOrCategory: genreOrCategoryReducer,
     currentUser: authUserReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(tmdbApi.middleware),
 });
+
+export default store;
